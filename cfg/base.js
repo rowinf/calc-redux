1fc34f977cbbd19d74a690a9d227693228d2f543
Rob Irwin <irwin.shell@gmail.com>
2016-05-08 00:01:37 +1200

    added redux w/yeoman-react-redux

--- cfg/base.js
+++ cfg/base.js
@@ -1,14 +1,7 @@
 'use strict';
 let path = require('path');
 let defaultSettings = require('./defaults');
-
-// Additional npm or bower modules to include in builds
-// Add all foreign plugins you may need into this array
-// @example:
-// let npmBase = path.join(__dirname, '../node_modules');
-// let additionalPaths = [ path.join(npmBase, 'react-bootstrap') ];
 let additionalPaths = [];
-
 module.exports = {
   additionalPaths: additionalPaths,
   port: defaultSettings.port,
@@ -17,7 +10,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, '/../dist/assets'),
     filename: 'app.js',
-    publicPath: `.${defaultSettings.publicPath}`
+    publicPath: `.${ defaultSettings.publicPath }`
   },
   devServer: {
     contentBase: './src/',
@@ -28,15 +21,22 @@ module.exports = {
     noInfo: false
   },
   resolve: {
-    extensions: ['', '.js', '.jsx'],
+    extensions: [
+      '',
+      '.js',
+      '.jsx'
+    ],
     alias: {
-      actions: `${defaultSettings.srcPath}/actions/`,
-      components: `${defaultSettings.srcPath}/components/`,
-      sources: `${defaultSettings.srcPath}/sources/`,
-      stores: `${defaultSettings.srcPath}/stores/`,
-      styles: `${defaultSettings.srcPath}/styles/`,
-      config: `${defaultSettings.srcPath}/config/` + process.env.REACT_WEBPACK_ENV
+      actions: `${ defaultSettings.srcPath }/actions/`,
+      components: `${ defaultSettings.srcPath }/components/`,
+      sources: `${ defaultSettings.srcPath }/sources/`,
+      stores: `${ defaultSettings.srcPath }/stores/`,
+      styles: `${ defaultSettings.srcPath }/styles/`,
+      config: `${ defaultSettings.srcPath }/config/` + process.env.REACT_WEBPACK_ENV
     }
   },
-  module: {}
+  module: {},
+  postcss: function () {
+    return [];
+  }
 };
